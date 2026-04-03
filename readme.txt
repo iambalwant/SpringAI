46c837005d76457b118d5160d543b626d4d516b2
iambalwant <balwantverma55555@gmail.com>
2026-04-03 21:31:33 +0530

    Chat Memory - jdbcMemory

---
+++ readme.txt
@@ -0,0 +1,3 @@
+ psql -U admin -d postgres
+\c admin
+SELECT * FROM spring_ai_yt.spring_ai_chat_memory
--- src/main/java/com/spring/ai/practice/config/AiConfig.java
+++ src/main/java/com/spring/ai/practice/config/AiConfig.java
@@ -9,8 +9,6 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.ollama.OllamaChatModel;
-//import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
